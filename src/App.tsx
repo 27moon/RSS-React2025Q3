13b41b50cce689b3d4b34bf33d3bf94e86fd595e
Marta <emeraldraven27@gmail.com>
2025-07-12 00:31:22 +0300

    feat: add basic setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const age: string = 'seventeen';
+  console.log(age);
+
+  return (
+    <>
+      <div>Project {age}</div>
+    </>
+  );
+}
+
+export default App;
