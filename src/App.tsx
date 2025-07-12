369483eaae3221c9efadcd1c4190226b99aaccc8
Marta <emeraldraven27@gmail.com>
2025-07-12 18:42:23 +0300

    feat: add basic header layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { Component } from 'react';
 import './App.css';
+import Header from './components/Header/header';
 
-function App() {
-  const age: string = 'seventeen';
-  console.log(age);
-
-  return (
-    <>
-      <div>Project {age}</div>
-    </>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <div>Project</div>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import Search from '../Search/search';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        header
+        <Search />
+      </header>
+    );
+  }
+}
+
+export default Header;
