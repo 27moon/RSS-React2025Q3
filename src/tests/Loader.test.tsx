e5ca2342e25fb1f18557634c0242812c3b115a93
Marta <emeraldraven27@gmail.com>
2025-07-18 20:32:31 +0300

    feat: add Loader test

---
+++ src/tests/Loader.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { Loader } from '../components/Loader/loader';
+
+describe('Loader', () => {
+  it('renders a loading gif', () => {
+    render(<Loader />);
+    const gif = screen.getByAltText('loading');
+    expect(gif).toBeInTheDocument();
+  });
+});
