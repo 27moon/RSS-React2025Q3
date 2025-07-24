c59be60897f97630b72789826e8d3bf12f78efbe
Marta <emeraldraven27@gmail.com>
2025-07-24 20:17:32 +0300

    feat: make Card, CardList, Loader functions

--- src/components/Loader/loader.tsx
+++ src/components/Loader/loader.tsx
@@ -1,12 +1,9 @@
-import { Component } from 'react';
 import load from '../../assets/load.gif';
 
-export class Loader extends Component {
-  render() {
-    return (
-      <div>
-        <img src={load} alt="loading" />
-      </div>
-    );
-  }
+export function Loader() {
+  return (
+    <div>
+      <img src={load} alt="loading" />
+    </div>
+  );
 }
